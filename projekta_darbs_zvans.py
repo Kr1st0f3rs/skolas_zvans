d83a4a531e334fc5a0332dcbca0dec701cb64867
Kr1st0f3rs <165793796+Kr1st0f3rs@users.noreply.github.com>
2024-05-02 19:42:06 +0300

    Add files via upload

---
+++ projekta_darbs_zvans.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import *
+
+#logs
+platums=720
+augstums=480
+logs=Tk()
+logs.title("Skolas zvans")
+canvas = Canvas(logs, width=platums, height=augstums)
+
+
+
+logs.mainloop()
