c06c4169282cbb8237c82e2127bdd98e009334ee
Kr1st0f3rs <165793796+Kr1st0f3rs@users.noreply.github.com>
2024-05-08 00:31:03 +0300

    Add files via upload

--- nosleguma_darbs_nordvigs.py
+++ nosleguma_darbs_nordvigs.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import *
-import time
+import time as time
 
 #logs
 logs=Tk()
@@ -19,6 +19,28 @@ zvans4=canvas.create_oval(375,225,425,275,fill="gray",outline="")
 zvans5=canvas.create_polygon(580,500,680,250,700,250,600,500,fill="gray",outline="")
 zvans6=canvas.create_oval(660,230,710,280,fill="red",outline="")
 
+#animacija
+def animacija(elapsed_time=0):
+    if elapsed_time < 110:
+        canvas.move(zvans6, -1, 0)
+        canvas.move(zvans5, -1, 0)
+        logs.after(1, lambda: animacija(elapsed_time + 1))
+    else:
+        pass
+
+def animacija_atpakal(elapsed_time=0):
+    if elapsed_time < 110:
+        canvas.move(zvans6, 1, 0)
+        canvas.move(zvans5, 1, 0)
+        logs.after(1, lambda: animacija_atpakal(elapsed_time + 1))
+    else:
+        pass
+
+# Define buttons to trigger animations
+animacija_poga = Button(logs, text="animacija", command=lambda: animacija())
+animacija_poga.place(x=700, y=600)
+animacija_poga2 = Button(logs, text="animacija_back", command=lambda: animacija_atpakal())
+animacija_poga2.place(x=700, y=500)
 
 
 logs.mainloop()
